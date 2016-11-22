964ead7ad066144317e78d6a6f56b195801c70e4
Ketan Saxena <ksaxena@grepruby.com>
2016-11-22 19:16:50 +0530

    Adds show action the users controller

--- market_place_api/spec/models/user_spec.rb
+++ market_place_api/spec/models/user_spec.rb
@@ -8,6 +8,6 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
   it { should be_valid }
-end
+
+end	
