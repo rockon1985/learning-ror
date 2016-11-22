bafac8595beb6a82c9cdf11f10ede811b3f65fc2
Ketan Saxena <ksaxena@grepruby.com>
2016-11-22 19:52:56 +0530

    Adds endpoint and spec for creating user

--- market_place_api/spec/controllers/api/v1/users_controller_spec.rb
+++ market_place_api/spec/controllers/api/v1/users_controller_spec.rb
@@ -13,7 +13,53 @@ describe Api::V1::UsersController do
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eql @user.email
     end
-    
-    it { should respond_with 200 }
+
+    it "should respond_with 200 http status" do
+      status_code = response.instance_variable_get(:@status)
+      expect(status_code).to eql 200
+    end
+  end
+
+  describe "POST #create" do
+    context "when user is successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it "renders the json representation for the user record just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it "should respond_with 201 http status" do
+        status_code = response.instance_variable_get(:@status)
+        expect(status_code).to eql 201
+      end
+    end
+
+    context "when user is not created" do
+      before(:each) do
+        #not including the email
+        @invalid_user_attributes = { password: "12345678",
+                                     password_confirmation: "12345678" }
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors]).to be_truthy
+      end
+
+      it "renders the json errors on why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it "should respond_with 422 http status" do
+        status_code = response.instance_variable_get(:@status)
+        expect(status_code).to eql 422
+      end
+    end
   end
 end
