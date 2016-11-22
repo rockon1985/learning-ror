e465b72067eae5de5f559bdde4abbc84806dea92
Ketan Saxena <ksaxena@grepruby.com>
2016-11-22 18:25:03 +0530

    Adds user firsts specs

--- market_place_api/config/routes.rb
+++ market_place_api/config/routes.rb
@@ -1,7 +1,13 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api, defaults: { format: :json },
-    constraints: { subdomain: 'api' }, path: '/'  do
-    # We are going to list our resources here
+    namespace :api, defaults: { format: :json },
+      constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+      constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
   end
 end
